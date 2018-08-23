69928d3d1cb5ab06c4f4036c718137117a670c18
Khaja Mohammed <kmoha023@fiu.edu>
2018-08-22 23:17:09 -0400

    restful app in go running successfully

---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Person struct {
+	ID        string   `json:"id,omitempty"`
+	Firstname string   `json:"firstname,omitempty"`
+	Lastname  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}
+
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
+
+var people []Person
+
+func GetPeople(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(people)
+}
+
+func GetPerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, item := range people {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Person{})
+}
+
+func CreatePerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var person Person
+	_ = json.NewDecoder(r.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
+}
+
+func DeletePerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for index, item := range people {
+		if item.ID == params["id"] {
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(people)
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	//Sample set Data (Use Database if available)
+	people = append(people, Person{ID: "1", Firstname: "Khaja", Lastname: "Mohammed", Address: &Address{City: "Atlanta", State: "Georgia"}})
+	people = append(people, Person{ID: "2", Firstname: "Akash", Lastname: "Bussa", Address: &Address{City: "St.Louis", State: "Missouri"}})
+	people = append(people, Person{ID: "3", Firstname: "Sam", Lastname: "Mathew", Address: &Address{City: "Richmond", State: "Virginia"}})
+
+	router.HandleFunc("/people", GetPeople).Methods("GET")
+	router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
+	router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
+	router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
+	log.Println("Starting server on port 6000")
+	err := http.ListenAndServe(":6000", router)
+	if err != nil {
+		log.Fatal("Error starting server", err)
+	}
+
+}
